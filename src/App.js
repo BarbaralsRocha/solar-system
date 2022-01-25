b5e6b9e42abd86dd5a447050044d04243f367272
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-25 15:30:14 -0300

    requisito2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <Header />
+        <SolarSystem />
+        <p>Sistema Solar</p>
+      </div>
+
+    );
   }
 }
 
